b54f92efbb10e28ed2d21603a7dc6a0562617808
Murphy Randle <murphy@murphys-macbook-pro.local>
2021-06-19 19:28:05 +0000

    Hacks to make flashes of content go away

--- Scripts/formatElixirDocument.js
+++ Scripts/formatElixirDocument.js
@@ -13,6 +13,9 @@ function formatElixirDocument(editor) {
       return editor
         .edit((editSession) => {
           editSession.replace(range, formattedDoc);
+          // It's strange that I need to repeat this here _and_ in the `then` below, but if I don't, I either get a bad cursor move, or I get a flash of changed content. It seems that if I do this in both places, all I get is a weird scroll effect.
+          editor.selectedRange = new Range(oldCursor.start, oldCursor.end);
+          editor.scrollToCursorPosition();
         })
         .then((a) => {
           editor.selectedRange = new Range(oldCursor.start, oldCursor.end);
